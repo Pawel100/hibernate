f4d6fa760ce899c99018214c6ca79890b1616c2d
Pawel100 <urbanpawel@o2.pl>
2018-05-03 14:27:57 +0200

    Teacher class added

---
+++ src/Teacher.java
@@ -0,0 +1,67 @@
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+
+public class Teacher implements java.io.Serializable{
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	@Column
+    private String name;
+	
+	@Column
+    private String subject;
+	
+	@ManyToMany(cascade=CascadeType.ALL)
+	@JoinColumn(name="class_id")
+    private Set<SchoolClass> classes;
+	
+	public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public Set<SchoolClass> getClasses() {
+        return classes;
+    }
+
+    public void setId(long id) {
+
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void setClasses(Set<SchoolClass> classes) {
+        this.classes = classes;
+    }
+    
+    @Override
+    public String toString() {
+        return "Teacher{" + "id=" + id + ", name='" + name + '\'' + ", subject='" + subject + '\'' + ", classes=" + classes + '}';
+    }
+    
+    
+
+}
